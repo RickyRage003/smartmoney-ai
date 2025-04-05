268c7bb4242e6bbbe486bedfd9d2f626a00f792d
RickyRage003 <deardest1ny@tuta.com>
2025-04-05 21:24:08 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -14,14 +14,13 @@ bot = Bot(token=TELEGRAM_TOKEN)
 async def send_telegram_report(filtered_csv_path, chart_path, accuracy):
     msg = f"✅ Фильтрованные сигналы\n🎯 Accuracy: {accuracy:.4f}"
 
-    async with bot:
-        # Отправляем текст
-        await bot.send_message(chat_id=CHAT_ID, text=msg)
+    # Отправляем текст
+    await bot.send_message(chat_id=CHAT_ID, text=msg)
 
-        # Отправляем график
-        with open(chart_path, "rb") as chart:
-            await bot.send_photo(chat_id=CHAT_ID, photo=InputFile(chart, filename="filter_chart.png"))
+    # Отправляем график
+    with open(chart_path, "rb") as chart:
+        await bot.send_photo(chat_id=CHAT_ID, photo=InputFile(chart, filename="filter_chart.png"))
 
-        # Отправляем CSV-файл
-        with open(filtered_csv_path, "rb") as csv_file:
-            await bot.send_document(chat_id=CHAT_ID, document=InputFile(csv_file, filename="filtered_signals.csv"))
+    # Отправляем CSV-файл
+    with open(filtered_csv_path, "rb") as csv_file:
+        await bot.send_document(chat_id=CHAT_ID, document=InputFile(csv_file, filename="filtered_signals.csv"))
