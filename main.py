5dabe98333c47b6f95d181f8d8cfaea9ce5aa229
RickyRage003 <deardest1ny@tuta.com>
2025-04-05 11:52:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from model.train_model import train_model
+import time
 
 if __name__ == "__main__":
     train_model()
+    while True:
+        time.sleep(3600)  # спим 1 час, чтобы Render не рестартовал процесс
