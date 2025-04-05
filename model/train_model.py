91f197616bccba3c72cc4022022321d50cf1782f
RickyRage003 <deardest1ny@tuta.com>
2025-04-05 17:53:19 +0300

    Update train_model.py

--- model/train_model.py
+++ model/train_model.py
@@ -1,27 +1,16 @@
-import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-def train_model():
-    df = pd.read_csv("data/sample_strategy.csv")
-
-    # Целевая переменная
-    y = df["result"]
-
-    # Исключаем нефичевые столбцы
-    exclude = ["timestamp", "symbol", "entry_price", "exit_price", "result"]
-    feature_columns = [col for col in df.columns if col not in exclude]
-
-    # Признаки
-    X = df[feature_columns]
-
-    # Обучение модели
+def train_model(X, y, base_df):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = RandomForestClassifier()
     model.fit(X_train, y_train)
+    accuracy = accuracy_score(y_test, model.predict(X_test))
 
-    accuracy = model.score(X_test, y_test)
-    print(f"Accuracy: {accuracy:.4f}")
+    # Предсказания и фильтрация
+    y_pred = model.predict(X)
+    base_df["prediction"] = y_pred
+    filtered = base_df[base_df["prediction"] == 1]
 
-    return model
+    return model, filtered, accuracy
